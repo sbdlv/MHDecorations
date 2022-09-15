9e95332a799285dd67abaf019455ec09b027b8dc
sbdlv <sergiobarriodelavega@gmail.com>
2022-09-15 20:05:27 +0200

    feat(shared): create api service

--- spa/src/app/shared/shared/shared.module.ts
+++ spa/src/app/shared/shared/shared.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentsModule } from '../components/components.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ApiService } from '../services/api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    HttpClientModule
   ],
   exports: [
     ComponentsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    ApiService
   ]
 })
 export class SharedModule { }
