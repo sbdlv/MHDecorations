9fe8cf21c9b53fbed0196e04fe77a776b82c84eb
sbdlv <sergiobarriodelavega@gmail.com>
2022-10-22 20:51:01 +0200

    feat(app.component): search the query in both source language's and target language's jewels data

--- spa/src/app/app.component.ts
+++ spa/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { IJewel, JewelsService } from './shared/services/jewels.service';
 
 interface QueriedData {
   id: string,
-  sourceName: string,
+  sourceName?: string,
   targetName?: string,
   desc?: string,
   ability?: string,
@@ -93,15 +93,29 @@ export class AppComponent implements OnInit {
   }
 
   onSearch() {
-    const lowerCaseQuery = this.query.toLowerCase();
-    const matchedJewels = this.sourceJewels.filter(jewel => jewel.name.toLocaleLowerCase().search(lowerCaseQuery) > -1)
+    const lowerCaseQuery = this.query.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 
-    this.queriedJewels = matchedJewels.map(matchedJewel => {
-      const targetLangJewel = this.targetJewels.find(jewel => jewel.id === matchedJewel.id);
+    // Search jewels from both languages
+    const matchedJewelsSourceLang = this.sourceJewels.filter(jewel =>
+      jewel.name.toLocaleLowerCase().search(lowerCaseQuery) > -1)
+    const matchedJewelsTargetLang = this.targetJewels.filter(jewel =>
+      jewel.name.toLocaleLowerCase().search(lowerCaseQuery) > -1)
+
+    // Create a collection with the matched jewels IDs form both languages
+    const joinedMatchedIDs = new Set<string>(
+      [
+        ...matchedJewelsSourceLang.map(jewel => jewel.id),
+        ...matchedJewelsTargetLang.map(jewel => jewel.id)
+      ]
+    );
+
+    // Get the data for the matched IDs
+    this.queriedJewels = Array.from(joinedMatchedIDs).map(matchedJewelID => {
+      const targetLangJewel = this.targetJewels.find(jewel => jewel.id === matchedJewelID);
 
       return {
-        id: matchedJewel.id,
-        sourceName: matchedJewel.name,
+        id: matchedJewelID,
+        sourceName: this.sourceJewels.find(jewel => jewel.id === matchedJewelID)?.name,
         targetName: targetLangJewel?.name,
         desc: targetLangJewel?.desc,
         ability: targetLangJewel?.ability
